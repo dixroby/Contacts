296b9296048059b16982013c537e38e972db8e3c
dixroby <dixroby.arone@touchconsulting.pe>
2024-07-10 12:59:54 -0500

    Agregar archivos de proyecto.

---
+++ Contacts.Maui/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace Contacts.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                Counterlbl.Text = $"Clicked {count} time";
+            else
+                Counterlbl.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
